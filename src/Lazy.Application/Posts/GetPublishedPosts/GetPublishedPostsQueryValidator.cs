84cb8ddd21133c5c2bc389aa8369d971e9655797
Serghei Adam <adam.serghei@gmail.com>
2023-02-21 23:13:34 +0200

    #?? - fixed negative offest issue for all posts

---
+++ src/Lazy.Application/Posts/GetPublishedPosts/GetPublishedPostsQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Lazy.Application.Posts.GetPublishedPosts;
+
+public class GetPublishedPostsQueryValidator : AbstractValidator<GetPublishedPostsQuery>
+{
+    public GetPublishedPostsQueryValidator()
+    {
+        RuleFor(x => x.Offset).GreaterThanOrEqualTo(0); 
+    }
+}
