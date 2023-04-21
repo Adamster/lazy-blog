1f7d8d0de2ef153600cf082d52fdea5625044614
Serghei Adam <serghei.adam@amdaris.com>
2023-04-21 12:45:35 +0300

    #22 - created post vote entity added configuration

---
+++ src/Lazy.Domain/Entities/PostVote.cs
@@ -0,0 +1,19 @@
+﻿using Lazy.Domain.Primitives;
+using Lazy.Domain.ValueObjects.Post;
+
+namespace Lazy.Domain.Entities;
+
+public class PostVote : Entity, IAuditableEntity
+{
+    public Guid PostId { get; private set; }
+    public Post Post { get; private set; }
+
+
+    public Guid UserId { get; private set; }
+    public User User { get; private set; }
+
+    public VoteDirection VoteDirection { get; private set; }
+
+    public DateTime CreatedOnUtc { get; set; }
+    public DateTime? UpdatedOnUtc { get; set; }
+}
---
+++ src/Lazy.Domain/ValueObjects/Post/VoteDirection.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Lazy.Domain.ValueObjects.Post;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum VoteDirection
+{
+    Up = 1,
+    Down = 2
+}
