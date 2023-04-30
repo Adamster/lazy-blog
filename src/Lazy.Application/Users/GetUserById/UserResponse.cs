e947a8370e2712e69d29743aee6aa14e12aadc50
Marius Lupasco <mariuslupasco16@gmail.com>
2023-04-30 14:22:28 +0300

    #?? - Made UserName and CreateOnUtc fields required in UserResponse model

--- src/Lazy.Application/Users/GetUserById/UserResponse.cs
+++ src/Lazy.Application/Users/GetUserById/UserResponse.cs
@@ -2,7 +2,7 @@
 
 namespace Lazy.Application.Users.GetUserById;
 
-public record UserResponse(Guid Id, string Email, string FirstName, string LastName, string? UserName, DateTime? CreatedOnUtc)
+public record UserResponse(Guid Id, string Email, string FirstName, string LastName, string UserName, DateTime CreatedOnUtc)
 {
     public UserResponse(User user) : this(user.Id, user.Email.Value, user.FirstName.Value, user.LastName.Value, user.UserName.Value, user.CreatedOnUtc)
     {
