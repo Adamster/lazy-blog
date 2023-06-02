1d9b51596f6c144b860d338b78fe791915fafc4d
Serghei Adam <serghei.adam@amdaris.com>
2023-06-02 10:47:28 +0300

    #?? - fixed post vote

--- src/Lazy.Domain/Entities/PostVote.cs
+++ src/Lazy.Domain/Entities/PostVote.cs
@@ -9,8 +9,8 @@ public class PostVote : Entity, IAuditableEntity
     public PostVote(Guid id, Post post, User user, VoteDirection voteDirection)
         : base(id)
     {
-        UserId = user.Id;
-        PostId = post.Id;
+        Post = post;
+        User = user;
         VoteDirection = voteDirection;
     }
 
