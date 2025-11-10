036e1a535bc898599dff0ee1d494b26fa1cde39f
Adamster <adamsterdomi@gmail.com>
2025-11-11 00:40:06 +0200

    Fixed forgot password controller access

--- src/Lazy.Blog.Presentation/Controllers/ForgotPasswordController.cs
+++ src/Lazy.Blog.Presentation/Controllers/ForgotPasswordController.cs
@@ -2,14 +2,16 @@ using Lazy.Application.Users.ForgotPassword;
 using Lazy.Presentation.Abstractions;
 using Lazy.Presentation.Contracts.Users;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Lazy.Presentation.Controllers;
 
-public class ForgotPasswordController(ISender sender, ILogger<ApiController> logger) : BaseJwtController(sender, logger)
+public class ForgotPasswordController(ISender sender, ILogger<ApiController> logger) : ApiController(sender, logger)
 {
+    [AllowAnonymous]
     [HttpPost("/forgot-password", Name = nameof(ForgotPassword))]
     [ProducesResponseType(StatusCodes.Status202Accepted)]
     public async Task<IActionResult> ForgotPassword([FromBody]ForgotPasswordRequest request)
